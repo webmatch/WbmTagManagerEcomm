c8e8f192661078467448a8282ef1fcc39a443760
Sebastian Basler <sbasler@webmatch.de>
2020-12-03 08:42:02 +0100

    [TASK] add click tracking for promotions

--- src/Resources/app/storefront/src/plugin/promotions.plugin.js
+++ src/Resources/app/storefront/src/plugin/promotions.plugin.js
@@ -4,35 +4,91 @@ import DomAccess from 'src/helper/dom-access.helper';
 export default class Promotions extends Plugin {
 
     init() {
-        const allPromotionObjects = DomAccess.querySelectorAll(this.el, '[data-promotion]');
-        let promotions = [];
+        let self = this;
+        let promotionObjects = DomAccess.querySelectorAll(this.el, '[data-promotion]');
+        console.log(promotionObjects);
 
-        allPromotionObjects.forEach(function (value, index) {
-            const dataSet = JSON.parse(JSON.stringify(value.dataset));
+        if (!promotionObjects) {
+            return;
+        }
+
+        this.promotions = [];
+
+        promotionObjects.forEach(function (object) {
+            const dataSet = JSON.parse(JSON.stringify(object.dataset));
 
             if (typeof dataSet.promotionId === 'undefined' && dataSet.promotionName === 'undefinded') {
                 return;
             }
 
-            let promotion = {
-                id: dataSet.promotionId || '',
-                name: dataSet.promotionName || '',
-                creative: dataSet.promotionCreative || '',
-                position: dataSet.promotionPosition || ''
-            }
+            self.addPromotion(dataSet);
+            self.registerEvent(object, dataSet);
 
-            promotions.push(promotion);
         });
 
-        window.dataLayer.push({
-            'event': 'promotions',
-            'ecommerce': {
-                'promoView': {
-                    'promotions': promotions
-                }
-            }
-        });
+        this.pushPromoView();
     }
 
+    registerEvent(object, dataSet) {
+        const self = this;
+        try {
+            const promotionLinks = DomAccess.querySelectorAll(object, 'a');
 
+            promotionLinks.forEach(function (link) {
+                link.addEventListener('click', function (event) {
+                    self.onPromotionClicked(event, link, dataSet)
+                })
+            })
+        } catch (e) {
+            // do nothing
+        }
+    }
+
+    onPromotionClicked(event, link, dataSet) {
+        event.preventDefault();
+
+        dataLayer.push({
+            'event': 'promotionClick',
+            'ecommerce': {
+                'promoClick': {
+                    'promotions': [
+                        {
+                            'id': dataSet.promotionId || '',
+                            'name': dataSet.promotionName || '',
+                            'creative': dataSet.promotionCreative || '',
+                            'position': dataSet.promotionPosition || ''
+                        }]
+                }
+            },
+            'eventCallback': function () {
+                document.location = DomAccess.getAttribute(link, 'href');
+            }
+        });
+
+    }
+
+    addPromotion(dataSet) {
+        let promotion = {
+            id: dataSet.promotionId || '',
+            name: dataSet.promotionName || '',
+            creative: dataSet.promotionCreative || '',
+            position: dataSet.promotionPosition || ''
+        }
+
+        this.promotions.push(promotion);
+    }
+
+    pushPromoView() {
+        console.log(this.promotions);
+        if (this.promotions.length > 0) {
+            window.dataLayer.push({
+                'event': 'promotions',
+                'ecommerce': {
+                    'promoView': {
+                        'promotions': this.promotions
+                    }
+                }
+            });
+        }
+    }
 }
