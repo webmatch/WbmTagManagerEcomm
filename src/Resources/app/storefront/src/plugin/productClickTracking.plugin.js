13682f7a82e41a2208008d39d406b25ecc267f22
Sebastian Basler <sbasler@webmatch.de>
2020-12-17 09:08:36 +0100

    [FIX] correct undefined impressions in product click tracking

--- src/Resources/app/storefront/src/plugin/productClickTracking.plugin.js
+++ src/Resources/app/storefront/src/plugin/productClickTracking.plugin.js
@@ -2,6 +2,8 @@ import Plugin from 'src/plugin-system/plugin.class';
 import DomAccess from 'src/helper/dom-access.helper';
 
 export default class ProductClickTracking extends Plugin {
+    impressions = null;
+
     init() {
         this.registerEvents();
     }
@@ -14,21 +16,25 @@ export default class ProductClickTracking extends Plugin {
     }
 
     setImpressions() {
-        const self = this;
-
-        let dataLayer = window.dataLayer;
+        let dataLayer = [
+            window.dataLayer
+        ];
         if (typeof onEventDataLayer !== 'undefined') {
-            dataLayer = onEventDataLayer;
+            dataLayer.push(onEventDataLayer);
         }
 
         if (dataLayer) {
-            for (let i = 0; i < window.dataLayer.length; i++) {
-                const layer = window.dataLayer[i];
+            for (let i = 0; i < dataLayer.length; i++) {
+                const layer = dataLayer[i];
 
                 if (layer.ecommerce && layer.ecommerce.impressions) {
-                    self.impressions = layer.ecommerce.impressions;
+                    this.impressions = layer.ecommerce.impressions;
                 }
             }
+        }
+
+        if (this.impressions === null) {
+            throw new InvalidImpressionsError('no impressions found');
         }
     }
 
@@ -37,31 +43,42 @@ export default class ProductClickTracking extends Plugin {
         if (DomAccess.hasAttribute(self.el, 'href')) {
             event.preventDefault();
         }
-        self.setImpressions();
+        try {
+            this.setImpressions();
 
-        const parent = self.el.closest('.product-box');
-        const inputField = DomAccess.querySelector(parent, '[itemprop="mpn"]');
-        const productNo = DomAccess.getAttribute(inputField, 'content');
-        const product = self.impressions.find((value, index) => {
-            return value.id === productNo;
-        });
+            const parent = self.el.closest('.product-box');
+            const inputField = DomAccess.querySelector(parent, '[itemprop="mpn"]');
+            const productNo = DomAccess.getAttribute(inputField, 'content');
+            const product = this.impressions.find((value, index) => {
+                return value.id === productNo;
+            });
 
-        if (product === undefined) {
-            return;
-        }
-
-        window.dataLayer.push({
-            event: 'productClick',
-            ecommerce: {
-                click: {
-                    actionField: { list: product.list },
-                    products: [product]
-                }
+            if (product === undefined) {
+                return;
             }
-        });
 
-        if (DomAccess.hasAttribute(self.el, 'href')) {
-            document.location = DomAccess.getAttribute(self.el, 'href');
+            window.dataLayer.push({
+                event: 'productClick',
+                ecommerce: {
+                    click: {
+                        actionField: {list: product.list},
+                        products: [product]
+                    }
+                }
+            });
+        } catch (e) {
+            // if something went wrong, just go on ...
         }
+
+        if (DomAccess.hasAttribute(this.el, 'href')) {
+            document.location = DomAccess.getAttribute(this.el, 'href');
+        }
+    }
+}
+
+class InvalidImpressionsError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidImpressionsError";
     }
 }
