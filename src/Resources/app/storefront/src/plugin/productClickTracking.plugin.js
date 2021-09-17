91670571bac1ed6758ef9e7b30637b40e0473ef2
Sebastian Basler <sbasler@webmatch.de>
2021-09-17 11:03:25 +0200

    [TASK][WBMSWPLUG-80] refactor code

--- src/Resources/app/storefront/src/plugin/productClickTracking.plugin.js
+++ src/Resources/app/storefront/src/plugin/productClickTracking.plugin.js
@@ -8,17 +8,17 @@ export default class ProductClickTracking extends Plugin {
         if (gtmIsTrackingProductClicks !== true) {
             return;
         }
-        this.registerEvents();
+        this._registerEvents();
     }
 
-    registerEvents() {
+    _registerEvents() {
         const self = this;
         this.el.addEventListener('click', (event) => {
             self.onProductClicked(event);
         });
     }
 
-    setImpressions() {
+    _setImpressions() {
         const dataLayer = [
             window.dataLayer
         ];
@@ -47,26 +47,10 @@ export default class ProductClickTracking extends Plugin {
         if (DomAccess.hasAttribute(this.el, 'href')) {
             event.preventDefault();
         }
-        let redirect = true;
 
         try {
-            this.setImpressions();
-
-            const parent = this.el.closest(this.options.parent);
-            console.log(parent.parentElement);
-            // enable quickview feature of SwagCmsExtension
-            if (parent.parentElement.dataset.swagCmsExtensionsQuickviewBox) {
-                redirect = false;
-            }
-            const inputField = DomAccess.querySelector(parent, '[itemprop="sku"]');
-            const productNo = DomAccess.getAttribute(inputField, 'content');
-            const product = this.impressions.find((value, index) => {
-                return value.id === productNo;
-            });
-
-            if (product === undefined) {
-                throw new InvalidImpressionsError('product not found in impressions');
-            }
+            this._setImpressions();
+            const product = this._getProduct();
 
             window.dataLayer.push({
                 event: 'productClick',
@@ -78,16 +62,43 @@ export default class ProductClickTracking extends Plugin {
                 }
             });
         } catch (e) {
+            console.log(e);
             // if something went wrong, just go on ...
         }
 
-        if (DomAccess.hasAttribute(this.el, 'href')) {
-            console.log(this.el);
-            console.log('wbm redirect');
-            if (redirect) {
-                document.location = DomAccess.getAttribute(this.el, 'href');
-            }
+        if (this._shouldRedirect()) {
+            document.location = DomAccess.getAttribute(this.el, 'href');
         }
+    }
+
+    _getProduct() {
+        const parent = this.el.closest(this.options.parent);
+        const inputField = DomAccess.querySelector(parent, '[itemprop="sku"]');
+        const productNo = DomAccess.getAttribute(inputField, 'content');
+        const product = this.impressions.find((value, index) => {
+            return value.id === productNo;
+        });
+
+        if (product === undefined) {
+            throw new InvalidImpressionsError('product not found in impressions');
+        }
+
+        return product
+    }
+
+    _shouldRedirect() {
+        let redirect = false;
+
+        // is there even a link?
+        if (DomAccess.hasAttribute(this.el, 'href')) {
+            redirect = true;
+        }
+        // enable quickview feature of SwagCmsExtension
+        if (this.el.closest(this.options.parent).parentElement.dataset.swagCmsExtensionsQuickviewBox) {
+            redirect = false;
+        }
+
+        return redirect;
     }
 }
 
